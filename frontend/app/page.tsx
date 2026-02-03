f309f9d3b153dfc5421c6af9d2dbc8e065d7efec
Ibnul Quayum Imran <iqimran@Ibnuls-MacBook-Pro.local>
2026-02-03 18:11:14 +0600

    section title updated & image position changed

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -290,10 +290,19 @@ export default function Home() {
 
         <section className="mb-10 pt-8">
           <h2 className="text-4xl font-bold text-center mb-10">
-            <span className="text-red-600">Patient's knowledge</span>{' '}
-            <span className="text-blue-500">Platform</span>
+            <span className="text-red-600">রিউমাটয়েড</span>{' '}
+            <span className="text-blue-500">আর্থ্রাইটিস</span>
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+            <Card className="bg-white rounded-2xl shadow-lg overflow-hidden hover:shadow-xl transition-shadow">
+              <div className="relative">
+                <Image
+                  width={615}
+                  height={400}
+                  src="/images/RA-Poster-2.jpg" alt="Education materials" className="object-cover" />
+              </div>
+              {/* <div className="p-6 flex justify-between items-center"> <h3 className="text-xl font-bold text-gray-800">Disease Educational Materials</h3> <Button className="bg-[#3C91C9] hover:bg-[#4FA3D1] text-white"> Learn more... </Button> </div> */}
+            </Card>
             <Card className="bg-white rounded-2xl shadow-lg overflow-hidden hover:shadow-xl transition-shadow"> 
                <div className="relative"> 
                 <Image
@@ -303,15 +312,7 @@ export default function Home() {
                </div>
               {/* <div className="p-6 flex justify-between items-center"> <h3 className="text-xl font-bold text-gray-800">Lifestyle Recommendation</h3> <Button className="bg-[#3C91C9] hover:bg-[#4FA3D1] text-white"> Learn more... </Button> </div> */}
             </Card>
-            <Card className="bg-white rounded-2xl shadow-lg overflow-hidden hover:shadow-xl transition-shadow"> 
-              <div className="relative"> 
-                <Image
-                  width={615}
-                  height={400}
-                  src="/images/RA-Poster-2.jpg" alt="Education materials" className="object-cover" /> 
-              </div>
-              {/* <div className="p-6 flex justify-between items-center"> <h3 className="text-xl font-bold text-gray-800">Disease Educational Materials</h3> <Button className="bg-[#3C91C9] hover:bg-[#4FA3D1] text-white"> Learn more... </Button> </div> */}
-            </Card>
+
             {/* <Card className="bg-white rounded-2xl shadow-lg overflow-hidden hover:shadow-xl transition-shadow"> <div className="relative h-64"> <Image src="https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Healthy recipe" fill className="object-cover" /> </div> <div className="p-6 flex justify-between items-center"> <h3 className="text-xl font-bold text-gray-800">Diet Recommendations for RA patients</h3> <Button className="bg-[#3C91C9] hover:bg-[#4FA3D1] text-white"> Learn more... </Button> </div> </Card> */}
             {/* <Card className="bg-white rounded-2xl shadow-lg overflow-hidden hover:shadow-xl transition-shadow"> <div className="relative h-64"> <Image src="https://images.pexels.com/photos/1557652/pexels-photo-1557652.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Reference" fill className="object-cover" /> </div> <div className="p-6 flex justify-between items-center"> <h3 className="text-xl font-bold text-gray-800">Reference</h3> <Button className="bg-[#3C91C9] hover:bg-[#4FA3D1] text-white"> Learn more... </Button> </div> </Card> */}
           </div>
